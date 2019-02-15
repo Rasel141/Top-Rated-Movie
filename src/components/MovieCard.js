be34b54f01800e298973d08f6970d5096f506e01
Rasel <rrasel141@gmail.com>
2019-02-15 19:24:13 +0600

    Add - movie card

---
+++ src/components/MovieCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class MovieCard extends Component {
+  render() {
+      const {movie} = this.props
+    return (
+      <div>
+        {movie.title}
+      </div>
+    )
+  }
+}
+
+export default MovieCard
