512fd788364c4b0780563bf343e5f92d52637343
Rasel <rrasel141@gmail.com>
2019-02-15 21:31:33 +0600

    Add - material app bar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+import './App.css'
 import apiKey from './apiKey'
 
 import MovieCard from './components/MovieCard'
 
-const originalMovies = [
-  {id: 1, title: 'Star War'},
-  {id: 2, title: 'Star Trek'},
-  {id: 3, title: 'Blade Runner'},
-]
 class App extends Component {
   state = {
     movies: []
   }
 
-  async componentDidMount() {
-    // setTimeout(() => this.setState({
-    //   movies: originalMovies
-    // }), 5000)
-    const response =  await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
+  async componentDidMount () {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`
+    )
     const json = await response.json()
-    this.setState({movies: json.results})
+    this.setState({ movies: json.results })
   }
 
-  render() {
-    const {movies} = this.state
+  render () {
+    const { movies } = this.state
+    console.log(movies)
     return (
-      <div className="App">
+      <div className='App'>
+        <AppBar position='fixed' >
+          <Toolbar>
+            <Typography variant='h6' color='inherit'>
+              Photos
+            </Typography>
+          </Toolbar>
+        </AppBar>
         Top Rated Movie
-        {movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
-
+        {movies.map(movie => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
