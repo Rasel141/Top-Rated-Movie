e3aeeedd33d70f6711359d914898df7f32b6b15a
Rasel <rrasel141@gmail.com>
2019-02-17 00:25:42 +0600

    Add - search functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import {Input, InputAdornment } from '@material-ui/core';
 
 import './App.css'
 import apiKey from './apiKey'
@@ -12,12 +13,26 @@ import MovieDialog from './components/MovieDialog'
 class App extends Component {
   state = {
     movies: [],
-    selectedMovie: null
+    selectedMovie: null,
+    searchText: ''
   }
 
   selectMovie = movie => this.setState({selectedMovie: movie})
   clearMovie = () => this.setState({selectedMovie: null})
 
+  searchTextChanged = e => this.setState({ searchText: e.target.value })
+
+  search = async e => {
+    e.preventDefault()
+
+    const {searchText} = this.state
+
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchText}`
+    )
+    const json = await response.json()
+    this.setState({ movies: json.results })
+  }
 
   async componentDidMount () {
     const response = await fetch(
@@ -28,15 +43,29 @@ class App extends Component {
   }
 
   render () {
-    const { movies, selectedMovie } = this.state
+    const { movies, selectedMovie, searchText } = this.state
     console.log(movies)
     return (
       <div className='App'>
         <AppBar position='fixed' >
           <Toolbar>
-            <Typography variant='h6' color='inherit'>
+            <Typography variant='h6' color='inherit' className="title">
             Top Rated Movie
             </Typography>
+            <form onSubmit={this.search}>
+            <Input
+                type="search"
+                value={searchText}
+                onChange={this.searchTextChanged}
+                startAdornment={
+                  <InputAdornment>
+                    <span role="img" aria-label="Search">
+                      🔍
+                    </span>
+                  </InputAdornment>
+                }
+              />
+            </form>
           </Toolbar>
         </AppBar>
       
